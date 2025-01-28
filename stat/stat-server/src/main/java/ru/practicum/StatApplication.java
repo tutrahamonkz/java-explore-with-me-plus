9bf993b232b13b85e1530cba0f23d1e542a5987d
tutrahamon <tutrachamon@mail.ru>
2025-01-28 13:22:09 +0300

    start stat-server

---
+++ stat/stat-server/src/main/java/ru/practicum/StatApplication.java
@@ -0,0 +1,9 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+
+public class StatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StatApplication.class, args);
+    }
+}
