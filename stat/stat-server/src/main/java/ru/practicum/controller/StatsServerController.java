27d041f61593ed1f0f4c28b744eb0d286cecb18b
AndreySanzharov <Andrsan2004@yandex.ru>
2025-01-30 11:47:19 +0300

    add controller

---
+++ stat/stat-server/src/main/java/ru/practicum/controller/StatsServerController.java
@@ -0,0 +1,33 @@
+package ru.practicum.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.dto.StatsDto;
+import ru.practicum.service.StatsService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class StatsServerController {
+
+    private final StatsService statsService;
+
+    @GetMapping("/stats")
+    @ResponseStatus(HttpStatus.OK)
+    public List<StatsDto> getStats(@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                   @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+                                   @RequestParam(defaultValue = "", required = false) List<String> uris,
+                                   @RequestParam(defaultValue = "false", required = false) boolean unique) {
+        return statsService.getStat(start, end, uris, unique);
+    }
+
+    @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
+    public StatsDto saveHitRequest(@RequestBody StatsDto statDto) {
+        return statsService.saveRequest(statDto);
+    }
+}
