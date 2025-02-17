4eb967335b3ebfadb0b3b7d086f13907502174ab
tutrahamon <tutrahamonkz@yandex.kz>
2025-02-17 20:12:19 +0300

    fix Data format

--- main-server/src/main/java/ru/practicum/comment/dto/CommentDto.java
+++ main-server/src/main/java/ru/practicum/comment/dto/CommentDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -19,6 +20,7 @@ public class CommentDto {
 
     private UserShortDto user;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime created = LocalDateTime.now();
 
     @NotNull(groups = CreateValidationGroup.class)
