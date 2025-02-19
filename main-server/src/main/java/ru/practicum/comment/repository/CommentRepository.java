171f38132aceb403edb40076df26a3bb758f3104
aperanto-lefko <polarster@yandex.ru>
2025-02-19 09:49:00 +0300

    Rename CommentServiceImpl, delete "public" from Repository

--- main-server/src/main/java/ru/practicum/comment/repository/CommentRepository.java
+++ main-server/src/main/java/ru/practicum/comment/repository/CommentRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    public List<Comment> findAllByEventId(Long eventId);
+    List<Comment> findAllByEventId(Long eventId);
 }
